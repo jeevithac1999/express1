1a982dae063236e68d87c73744d815e0243626b9
jeevithac1999 <59138170+jeevithac1999@users.noreply.github.com>
2020-02-01 12:49:29 +0530

    express1
    Learned express

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ app.get("/", (request, response) => {
   response.send("hello");
 });
 app.get("/profile", (request, response) => {
-  response.send("profile");
+  response.send("profile ");
 });
 const server = app.listen("8080", () => {
   console.log(`Server Running on port: ${server.address().port}.`);
